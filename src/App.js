458bab0c18b13fc845864d6d4aaf080810e6fcc8
Omarfaruk4200 <emon35293@gmail.com>
2022-10-18 12:27:16 +0600

    added home page and nav

--- src/App.js
+++ src/App.js
@@ -4,13 +4,9 @@ import Home from './Pages/Home/Home'
 
 function App() {
 
-
   return (
-
     <div>
-      {/* <h1 className='text-primary'>This is primary</h1> */}
       <Home />
-
     </div>
   )
 }
